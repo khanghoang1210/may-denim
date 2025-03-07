cc823bfe5cdcabbb7556f9cad00f5e26ab26f6b0
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2025-03-07 17:02:02 +0700

    fix request body

--- order-service/src/main/java/com/khanghoang/maydenim/order/controller/OrderController.java
+++ order-service/src/main/java/com/khanghoang/maydenim/order/controller/OrderController.java
@@ -4,10 +4,7 @@ import com.khanghoang.maydenim.order.dto.CreateOrderReq;
 import com.khanghoang.maydenim.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/orders")
@@ -17,7 +14,7 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(CreateOrderReq req){
+    public String placeOrder(@RequestBody CreateOrderReq req){
         orderService.placeOrder(req);
         return "Order Place Successfully";
     }
