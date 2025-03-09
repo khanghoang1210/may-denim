8085e5a58f6f5b14121b824bc7f83edbd5ee2cff
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2025-03-09 17:43:34 +0700

    ui navbar

--- frontend/components/layout/Navbar/index.tsx
+++ frontend/components/layout/Navbar/index.tsx
@@ -1,12 +1,99 @@
 import React from 'react'
+import Image from "next/image";
 import CartBtn from './CartBtn'
 import AccountBtn from './AccountBtn'
+import InputGroup from "@/components/ui/input-group";
+import { NavigationMenu, NavigationMenuList } from '@/components/ui/navigation-menu';
+import { MenuItem } from './MenuItem';
+import { MenuList } from './MenuList';
 
+const data = [
+    {
+        id: 1,
+        label: "Shop",
+        type: "MenuList",
+        children: [
+          {
+            id: 11,
+            label: "Men's clothes",
+            url: "/shop#men-clothes",
+            description: "In attractive and spectacular colors and designs",
+          },
+          {
+            id: 12,
+            label: "Women's clothes",
+            url: "/shop#women-clothes",
+            description: "Ladies, your style and tastes are important to us",
+          },
+          {
+            id: 13,
+            label: "Kids clothes",
+            url: "/shop#kids-clothes",
+            description: "For all ages, with happy and beautiful colors",
+          },
+          {
+            id: 14,
+            label: "Bags and Shoes",
+            url: "/shop#bag-shoes",
+            description: "Suitable for men, women and all tastes and styles",
+          },
+        ],
+      },
+    {
+      id: 2,
+      type: "MenuItem",
+      label: "On Sale",
+      url: "/shop#on-sale",
+      children: [],
+    },
+    {
+      id: 3,
+      type: "MenuItem",
+      label: "New Arrivals",
+      url: "/shop#new-arrivals",
+      children: [],
+    },
+    {
+      id: 4,
+      type: "MenuItem",
+      label: "Brands",
+      url: "/shop#brands",
+      children: [],
+    },
+  ];
 const Navbar = () => {
   return (
     <nav className='top-0 z-20 sticky'>
-        <div className='flex relative max-w-frame mx-auto justify-between px-4 py-5 items-center'>
+        <div className='flex relative max-w-7xl mx-auto justify-between items-center  py-5  px-4 '>
             <p>May Denim</p>
+
+            <NavigationMenu className="hidden md:flex mr-5 lg:mr-7">
+                <NavigationMenuList>
+                {data.map((item) => (
+                    <React.Fragment key={item.id}>
+                        <MenuItem label={item.label} url={item.url} />
+                    </React.Fragment>
+                ))}
+                </NavigationMenuList>
+            </NavigationMenu>
+            <InputGroup className="hidden md:flex bg-gray-10 mr-3 lg:mr-10">
+                <InputGroup.Text>
+                    <Image
+                    priority
+                    src="/icons/search.svg"
+                    height={20}
+                    width={20}
+                    alt="search"
+                    className="min-w-5 min-h-5"
+                    />
+                </InputGroup.Text>
+                <InputGroup.Input
+                    type="search"
+                    name="search"
+                    placeholder="Search"
+                    className="bg-transparent placeholder:text-black/40"
+                />
+            </InputGroup>
             <div className='flex items-end'>
                 <AccountBtn/>
                 <CartBtn/>
