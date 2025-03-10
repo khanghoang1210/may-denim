5d2c89003053e27b70df23e49a4e69b9759dd2b9
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2025-03-10 19:08:20 +0700

    add api gateway

---
+++ api-gateway/src/test/java/com/khanghoang/maydenim/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanghoang.maydenim.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
